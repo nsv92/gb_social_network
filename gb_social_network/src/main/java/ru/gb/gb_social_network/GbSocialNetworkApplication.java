9768bc74a0d17bdc77a9a2d367fffa38cdcc2072
Sergei Natalchuk <svnat92@gmail.com>
2022-11-26 18:10:17 +0300

    init

---
+++ gb_social_network/src/main/java/ru/gb/gb_social_network/GbSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.gb_social_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbSocialNetworkApplication.class, args);
+    }
+
+}
---
+++ gb_social_network/src/test/java/ru/gb/gb_social_network/GbSocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gb_social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbSocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

eaea7d991455295fb0405f68b23c56f478514a9d
Sergei Natalchuk <svnat92@gmail.com>
2022-12-20 02:13:15 +0300

    Попытка убрать конфликты №2

--- gb_social_network/backend/src/main/java/ru/gb/backend/services/UserService.java
+++ gb_social_network/backend/src/main/java/ru/gb/backend/services/UserService.java
@@ -1,12 +1,20 @@
 package ru.gb.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gb.backend.entity.Role;
 import ru.gb.backend.entity.User;
 import ru.gb.backend.repositories.UserRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -29,5 +37,20 @@ public class UserService {
     public User getUserForIds(Long id){
         return userRepository.findById(id)
                 .orElse(new User());
+    }
+
+    public Optional<User> findByNickname(String nickname) {
+        return userRepository.findByNickName(nickname);
+    }
+
+//    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+        User user = findByNickname(nickname).orElseThrow(() -> new UsernameNotFoundException(String.format("User '%s' not found", nickname)));
+        return new org.springframework.security.core.userdetails.User(user.getNickName(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
